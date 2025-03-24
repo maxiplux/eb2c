edca190cbb5acc0bc9ddae0b4afe152b625b8225
Juan Francisco Mosquera <maxiplux@gmail.com>
2025-03-23 22:38:55 -0400

    Add test configuration and mock dependencies for testing
    Introduced a test application, JPA, and MVC configurations to set up isolated test contexts with mocked dependencies. Added a .env file for environment variables along with the necessary changes to .gitignore to include it in the repository.

---
+++ src/test/java/app/quantun/eb2c/config/TestJpaConfig.java
@@ -0,0 +1,65 @@
+package app.quantun.eb2c.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Optional;
+import java.util.Properties;
+
+@TestConfiguration
+@EnableJpaAuditing(auditorAwareRef = "testAuditorProvider")
+@EnableJpaRepositories(basePackages = "app.quantun.eb2c.repository")
+@EnableTransactionManagement
+public class TestJpaConfig {
+
+    @Bean
+    public AuditorAware<String> testAuditorProvider() {
+        return () -> Optional.of("test-user");
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        jpaProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("app.quantun.eb2c.model.entity");
+        factory.setDataSource(dataSource());
+        factory.setJpaProperties(jpaProperties);
+        return factory;
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return txManager;
+    }
+} 
