75099de6f72b80d0488834688e75d62b29dde294
Juan Francisco Mosquera <maxiplux@gmail.com>
2025-03-24 06:45:05 -0400

    Use @MockitoBean instead of @MockBean in tests
    Replaced the @MockBean annotation with @MockitoBean for the CognitoGroupService in GroupControllerTest. This ensures better alignment with the updated testing framework configuration and improves test reliability.

--- src/test/java/app/quantun/eb2c/rest/GroupControllerTest.java
+++ src/test/java/app/quantun/eb2c/rest/GroupControllerTest.java
@@ -13,9 +13,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
@@ -39,7 +40,7 @@ public class GroupControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private CognitoGroupService groupService;
 
     @Autowired
