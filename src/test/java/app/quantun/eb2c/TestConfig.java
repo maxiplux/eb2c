f6a0eb0d65311817e6e9a3a6b3bf9835dbb51ced
Juan Francisco Mosquera <maxiplux@gmail.com>
2025-03-25 21:31:27 -0400

    Consolidate test configurations and streamline testing setup
    Combined `TestJpaConfig`, `TestMvcConfig`, and `TestSecurityConfig` into `TestConfig` for a more unified configuration. Introduced TestContainers dependencies to enhance test environment capabilities. Improved GitHub Actions workflow by adding detailed test reporting and result uploads.

--- src/test/java/app/quantun/eb2c/TestConfig.java
+++ src/test/java/app/quantun/eb2c/TestConfig.java
@@ -5,12 +5,24 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAu
 import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.util.Optional;
+import java.util.Properties;
 
 @Configuration
 @EnableAutoConfiguration(exclude = {
@@ -19,6 +31,10 @@ import java.util.Optional;
         OAuth2ClientAutoConfiguration.class,
         OAuth2ResourceServerAutoConfiguration.class
 })
+@TestConfiguration
+@EnableJpaAuditing(auditorAwareRef = "testAuditorProvider")
+@EnableJpaRepositories(basePackages = "app.quantun.eb2c.repository")
+@EnableTransactionManagement
 public class TestConfig {
     @Bean
     @Primary
@@ -33,4 +49,40 @@ public class TestConfig {
     }
 
 
-} 
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        jpaProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("app.quantun.eb2c.model.entity");
+        factory.setDataSource(dataSource());
+        factory.setJpaProperties(jpaProperties);
+        return factory;
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return txManager;
+    }
+
+
+}
